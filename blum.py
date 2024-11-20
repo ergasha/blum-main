c70f2681f1c0cd296485a24097b74e5ff2c773c2
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-11-20 06:54:14 +0500

    commit

--- blum.py
+++ blum.py
@@ -618,10 +618,10 @@ def find_by_id(json_data, id):
 
 def main():
 
-    claim_ref_enable = input("want claim ref? y/n  : ").strip().lower()      
+    claim_ref_enable = 'y'      
     # check_task_enable = input("want claim task? y/n  : ").strip().lower()
     check_task_enable = 'n'
-    selector_game = input("auto playing game ? y/n  : ").strip().lower()
+    selector_game = 'y'
     total_blum = 0
     while True:
         delete_all()
@@ -994,7 +994,7 @@ def start():
         2. Clear Task
           
           """)
-    selector = input("Select ( 1 / 2 )  : ").strip().lower()
+    selector = '1'
 
     if selector == '1':
         main()
